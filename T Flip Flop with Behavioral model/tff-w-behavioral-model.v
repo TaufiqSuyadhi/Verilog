7b7c3ca41f5a3b46f310fd6fe4671866692848e4
Taufiq Dwi Septian Suyadhi <robotics.university@gmail.com>
2026-09-16 19:01:24 +0700

    Add files via upload

---
+++ T Flip Flop with Behavioral model/tff-w-behavioral-model.v
@@ -0,0 +1,19 @@
+// Verilog code to implement T FlipFlop using BEHAVIORAL modeling
+
+module t(q,t,clk);
+output reg q:
+input t,clk;
+
+initial
+begin
+q=0;
+end
+
+always @(posedge clk)
+begin
+case (t)
+1'b0: q <= q;
+1'bl: q <= q;
+endcase
+end
+endmodule
