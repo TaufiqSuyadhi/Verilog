07beff2c8b9b49639b65c4d04c0269ca893be5b0
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-16 12:37:59 +0700

    Create 2bit-magnitude-comparator-w-data-flow.v

---
+++ Comparator with Data Flow/2bit-magnitude-comparator-w-data-flow.v
@@ -0,0 +1,15 @@
+// Verilog code to implement 2-bit Magnitude comparator using dataflow modeling
+
+module comparator_dataflow(G,E,L,A,B );
+input [1:0]A,B;
+output G; //A>B
+output E; //A=Boutput L; //A<B
+
+wire x1,x0;
+
+assign x1=~(A[1]^B[1]);
+assign x0=~(A[0]^B[0]);
+assign G=(A[1]&~B[1])|(x1&A[0]&B[0]);
+assign E=x1&x0;
+assign L=(~A[1]&B[1])|(x1&~A[0]&B[0]);
+endmodule
