5ab7d636101ffcbb95d8a3856dafa48e7bbe0d36
Taufiq Dwi Septian Suyadhi <robotics.university@gmail.com>
2026-09-17 07:35:20 +0700

    Add files via upload

---
+++ Basic Logic NOR Gate with Switch Level/tb-nor-gate-w-switch-level-modeling.v
@@ -0,0 +1,19 @@
+// Verilog testbench code to simulation NOR using Switch-level modeling
+// CMOS based modeling
+
+module cmos_nor_tb();
+wire y;
+reg a, b;
+cmos_nor a1 (y, a, b);
+initial
+begin
+$display("Robonesia 2026“)
+$display(" a | b | y ");
+$display(" ");
+a = 0; b = 0; #10 $display("%b %b | %b", a, b, y);
+a = 0; b = 1; #10 $display("%b %b | %b", a, b, y);
+a = 1; b = 0; #10 $display("%b %b | %b", a, b, y);
+a = 1; b = 1; #10 $display("%b %b | %b", a, b, y);
+$finish();
+end
+endmodule
