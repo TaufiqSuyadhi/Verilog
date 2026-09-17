627648c3cf14394a07324197d66daab926078482
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-17 07:35:01 +0700

    Create nor-gate-w-switch-level-modeling.v

---
+++ Basic Logic NOR Gate with Switch Level/nor-gate-w-switch-level-modeling.v
@@ -0,0 +1,14 @@
+// Verilog code to implement NOR using Switch-level modeling
+// CMOS based modeling
+
+module cmos_nor (y, a, b);
+output y;
+input a, b;
+supply1 vdd;
+supply0 gnd;
+wire e1;
+pmos p1 (y, e1, a);
+pmos p2 (e1, vdd, b);
+nmos n1_1 (y, gnd, a);
+nmos n2_1 (y, gnd, b);
+endmodule
