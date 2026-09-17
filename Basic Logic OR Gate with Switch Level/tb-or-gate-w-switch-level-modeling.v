4aa31d9d2458f20a3ae17ffa921640be00f37b25
Taufiq Dwi Septian Suyadhi <robotics.university@gmail.com>
2026-09-17 07:31:42 +0700

    Add files via upload

---
+++ Basic Logic OR Gate with Switch Level/tb-or-gate-w-switch-level-modeling.v
@@ -0,0 +1,19 @@
+// Verilog testbench code to simulation OR using Switch-level modeling
+// CMOS based modeling
+
+module cmos_or_tb();
+wire y;
+reg a, b;
+cmos_or a1 (y, a, b);
+initial
+begin
+$display(“Robonesia 2026”)
+$display(" a | b | y");
+$display(" ----------- ");
+a = 0; b = 0; #10 $display("%b %b | %b", a, b, y);
+a = 0; b = 1; #10 $display("%b %b | %b", a, b, y);
+a = 1; b = 0; #10 $display("%b %b | %b", a, b, y);
+a = 1; b = 1; #10 $display("%b %b | %b", a, b, y);
+$finish();
+end
+endmodule
