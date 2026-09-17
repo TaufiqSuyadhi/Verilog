ceaeaaca4a689dec631654f59801d843d1372880
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-17 07:33:16 +0700

    Create nand-gate-w-switch-level-modeling.v

---
+++ Basic Logic NAND Gate with Switch Level/nand-gate-w-switch-level-modeling.v
@@ -0,0 +1,14 @@
+// Verilog code to implement NAND using Switch-level modeling
+// CMOS based modeling
+
+module cmos_nand (y, a, b);
+output y;
+input a, b;
+supply1 vdd;
+supply0 gnd;
+wire e1;
+pmos p1 (y, vdd, a);
+pmos p2 (y, vdd, b);
+nmos n1 (y, e1, b);
+nmos n2 (e1, gnd, a);
+endmodule
