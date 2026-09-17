e1d686e9fbdf58357a0322427b835d790fa86c1c
Taufiq Dwi Septian Suyadhi <robotics.university@gmail.com>
2026-09-17 07:33:38 +0700

    Add files via upload

---
+++ Basic Logic NAND Gate with Switch Level/tb-nand-gate-w-switch-level-modeling.v
@@ -0,0 +1,19 @@
+// Verilog testbench code to simulation NAND using Switch-level modeling
+// CMOS based modeling
+
+module cmos_nand_tb();
+wire y;
+reg a, b;
+cmos_nand a1 (y, a, b);
+initial
+begin
+$display("Robonesia 2026 ");
+$display(" a|b|y");
+$display(" ----------- ");
+a = 0; b = 0; #10 $display("%b %b | %b", a, b, y);
+a = 0; b = 1; #10 $display("%b %b | %b", a, b, y);
+a = 1; b = 0; #10 $display("%b %b | %b", a, b, y);
+a = 1; b = 1; #10 $display("%b %b | %b", a, b, y);
+$finish();
+end
+endmodule
