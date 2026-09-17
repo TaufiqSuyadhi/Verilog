fedc8d45c0b579ccfe2eb774f9bb2bebf14c5958
Taufiq Dwi Septian Suyadhi <robotics.university@gmail.com>
2026-09-17 07:36:56 +0700

    Add files via upload

---
+++ Basic Logic XOR Gate with Switch Level/tb-xor-gate-w-switch-level-modeling.v
@@ -0,0 +1,19 @@
+// Verilog testbench code to simulation XOR using Switch-level modeling
+// CMOS based modeling
+
+module cmos_xor_tb();
+reg a,b;
+wire y;
+cmos_xor t1(y,a,b);
+initial
+begin
+$display("Robonesia 2026");
+$display("a b | y");
+$display(" -------");
+a=0;b=0;#10 $display("%b %b | %b", a,b,y);
+a=0;b=1;#10 $display("%b %b | %b", a,b,y);
+a=1;b=0;#10 $display("%b %b | %b", a,b,y);
+a=1;b=1;#10 $display("%b %b | %b", a,b,y);
+$finish();
+end
+endmodule
