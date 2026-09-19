5581611b1d467f34f0dff63a7450a8151b7c7eba
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-19 13:08:35 +0700

    Update digital-stopwatch.v

--- Project 2 - Digital Stopwatch using Verilog/digital-stopwatch.v
+++ Project 2 - Digital Stopwatch using Verilog/digital-stopwatch.v
@@ -1,11 +1,11 @@
 // Verilog code for digital_stopwatch application
 
 module digital_stopwatch (
-input clk, // 1 Hz clock
-input reset,
-input start,
-output reg [5:0] seconds,
-output reg [5:0] minutes
+	input clk, // 1 Hz clock
+	input reset,
+	input start,
+	output reg [5:0] seconds,
+	output reg [5:0] minutes
 );
 
 always @(posedge clk or posedge reset) begin
@@ -13,7 +13,6 @@ if (reset) begin
 	seconds <= 6'd0;
 	minutes <= 6'd0;
 end
-
 else if (start) begin
 if (seconds == 6'd59) begin
 	seconds <= 6'd0;
