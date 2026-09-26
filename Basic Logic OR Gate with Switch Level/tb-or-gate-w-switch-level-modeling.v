874dd0793adb997b4aa2b39be2b0568210649ea2
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-26 11:11:45 +0700

    Update tb-or-gate-w-switch-level-modeling.v

--- Basic Logic OR Gate with Switch Level/tb-or-gate-w-switch-level-modeling.v
+++ Basic Logic OR Gate with Switch Level/tb-or-gate-w-switch-level-modeling.v
@@ -1,19 +1,22 @@
-// Verilog testbench code to simulation OR using Switch-level modeling
-// CMOS based modeling
+// Verilog testbench code to simulate OR gate using Switch-level modeling
 
 module cmos_or_tb();
 wire y;
 reg a, b;
+  
 cmos_or a1 (y, a, b);
+  
 initial
+  
 begin
-$display(“Robonesia 2026”)
-$display(" a | b | y");
-$display(" ----------- ");
-a = 0; b = 0; #10 $display("%b %b | %b", a, b, y);
-a = 0; b = 1; #10 $display("%b %b | %b", a, b, y);
-a = 1; b = 0; #10 $display("%b %b | %b", a, b, y);
-a = 1; b = 1; #10 $display("%b %b | %b", a, b, y);
-$finish();
+  $display(“Robonesia 2026”)
+  $display(" a | b | y");
+  $display(" ----------- ");
+  a = 0; b = 0; #10 $display("%b %b | %b", a, b, y);
+  a = 0; b = 1; #10 $display("%b %b | %b", a, b, y);
+  a = 1; b = 0; #10 $display("%b %b | %b", a, b, y);
+  a = 1; b = 1; #10 $display("%b %b | %b", a, b, y);
+  $finish();
 end
+  
 endmodule
