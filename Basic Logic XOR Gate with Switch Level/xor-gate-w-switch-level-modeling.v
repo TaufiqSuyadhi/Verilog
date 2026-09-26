28729fdd8328b4aafca9cbdcafa93aa3f7f0f763
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-26 11:18:49 +0700

    Update xor-gate-w-switch-level-modeling.v

--- Basic Logic XOR Gate with Switch Level/xor-gate-w-switch-level-modeling.v
+++ Basic Logic XOR Gate with Switch Level/xor-gate-w-switch-level-modeling.v
@@ -1,15 +1,19 @@
-// Verilog code to implement XOR using Switch-level modeling
-// CMOS based modeling
+// Verilog code to implement XOR logic gate using Switch-level modeling
 
 module cmos_xor(y,a,b);
+
 input a,b;
 output y;
+  
 wire e1,e2;
 wire Abar,Bbar;
+  
 assign Abar=~a;
 assign Bbar=~b;
+  
 supply1 vdd;
 supply0 gnd;
+  
 pmos p1(e1,vdd,a);
 pmos p2(e1,vdd,b);
 pmos p3(y,e1,Abar);
@@ -18,4 +22,5 @@ nmos n1(e2,gnd,b);
 nmos n2(y,e2,a);
 nmos n3(e3,gnd,Bbar);
 nmos n4(y,e3,Abar);
+
 endmodule
