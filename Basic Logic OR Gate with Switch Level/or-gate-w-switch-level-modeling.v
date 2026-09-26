3977e79a3bac22f0174143792ac764efa66e0f33
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-26 11:10:44 +0700

    Update or-gate-w-switch-level-modeling.v

--- Basic Logic OR Gate with Switch Level/or-gate-w-switch-level-modeling.v
+++ Basic Logic OR Gate with Switch Level/or-gate-w-switch-level-modeling.v
@@ -1,17 +1,19 @@
-// Verilog code to implement OR using Switch-level modeling
-// CMOS based modeling
+// Verilog code to implement OR gate using Switch-level modeling
 
 module cmos_or (y, a, b);
 output y;
 input a, b;
+  
 wire e1;
 supply1 vdd;
 supply0 gnd;
 wire e2;
+  
 pmos p1_1 (e1, e2, a);
 pmos p2_1 (e2, vdd, b);
 nmos n1 (e1, gnd, a);
 nmos n2 (e1, gnd, b);
 pmos p3 (y, vdd, e1);
 nmos n4 (y, gnd, e1);
+  
 endmodule
