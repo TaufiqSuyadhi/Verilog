06b904dd5adaa45944a9a91cd04050e30da4682c
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-26 11:20:02 +0700

    Update tb-xor-gate-w-switch-level-modeling.v

--- Basic Logic XOR Gate with Switch Level/tb-xor-gate-w-switch-level-modeling.v
+++ Basic Logic XOR Gate with Switch Level/tb-xor-gate-w-switch-level-modeling.v
@@ -1,19 +1,23 @@
-// Verilog testbench code to simulation XOR using Switch-level modeling
-// CMOS based modeling
+// Verilog testbench code to simulate XOR logic gate using Switch-level modeling
 
 module cmos_xor_tb();
+  
 reg a,b;
 wire y;
+  
 cmos_xor t1(y,a,b);
+  
 initial
+  
 begin
-$display("Robonesia 2026");
-$display("a b | y");
-$display(" -------");
-a=0;b=0;#10 $display("%b %b | %b", a,b,y);
-a=0;b=1;#10 $display("%b %b | %b", a,b,y);
-a=1;b=0;#10 $display("%b %b | %b", a,b,y);
-a=1;b=1;#10 $display("%b %b | %b", a,b,y);
-$finish();
+  $display("Robonesia 2026");
+  $display("a b | y");
+  $display(" -------");
+  a=0;b=0;#10 $display("%b %b | %b", a,b,y);
+  a=0;b=1;#10 $display("%b %b | %b", a,b,y);
+  a=1;b=0;#10 $display("%b %b | %b", a,b,y);
+  a=1;b=1;#10 $display("%b %b | %b", a,b,y);
+  $finish();
 end
+  
 endmodule
