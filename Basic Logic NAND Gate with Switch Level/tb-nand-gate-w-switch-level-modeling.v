6269d3fc94cd6a5ad29bf5244dafcbea446c71f2
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-26 11:14:31 +0700

    Update tb-nand-gate-w-switch-level-modeling.v

--- Basic Logic NAND Gate with Switch Level/tb-nand-gate-w-switch-level-modeling.v
+++ Basic Logic NAND Gate with Switch Level/tb-nand-gate-w-switch-level-modeling.v
@@ -1,19 +1,22 @@
-// Verilog testbench code to simulation NAND using Switch-level modeling
-// CMOS based modeling
+// Verilog testbench code to simulate NAND gate using Switch-level modeling
 
 module cmos_nand_tb();
 wire y;
 reg a, b;
+  
 cmos_nand a1 (y, a, b);
+  
 initial
+  
 begin
-$display("Robonesia 2026 ");
-$display(" a|b|y");
-$display(" ----------- ");
-a = 0; b = 0; #10 $display("%b %b | %b", a, b, y);
-a = 0; b = 1; #10 $display("%b %b | %b", a, b, y);
-a = 1; b = 0; #10 $display("%b %b | %b", a, b, y);
-a = 1; b = 1; #10 $display("%b %b | %b", a, b, y);
-$finish();
+  $display("Robonesia 2026 ");
+  $display(" a|b|y");
+  $display(" ----------- ");
+  a = 0; b = 0; #10 $display("%b %b | %b", a, b, y);
+  a = 0; b = 1; #10 $display("%b %b | %b", a, b, y);
+  a = 1; b = 0; #10 $display("%b %b | %b", a, b, y);
+  a = 1; b = 1; #10 $display("%b %b | %b", a, b, y);
+  $finish();
 end
+  
 endmodule
