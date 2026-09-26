004da7d057d77d5eec5a383f450186df7a2262a8
Taufiq Dwi Septian Suyadhi <taufiq.suyadhi@gmail.com>
2026-09-26 11:04:34 +0700

    Update and-gate-w-switch-level-modeling.v

--- Basic Logic AND Gate with Switch Level/and-gate-w-switch-level-modeling.v
+++ Basic Logic AND Gate with Switch Level/and-gate-w-switch-level-modeling.v
@@ -1,17 +1,19 @@
 // Verilog code to implement AND using Switch-level modeling
-// CMOS based modeling
 
 module cmos_and (y, a, b);
 output y;
 input a, b;
+  
 wire e1;
 supply1 vdd;
 supply0 gnd;
 wire e2;
+  
 pmos p1 (e1, vdd, a);
 pmos p2 (e1, vdd, b);
 nmos n2 (e1, e2, a);
 nmos n3 (e2, gnd, b);
 pmos p3 (y, vdd, e1);
 nmos n4 (y, gnd, e1);
+  
 endmodule
